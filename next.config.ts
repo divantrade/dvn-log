2c2a6a60e34bc70965486ce106ad2604d7bd8980
Adelsolimaan <info@divantrade.net>
2025-08-30 17:04:44 +0300

    fix: downgrade tailwindcss v4 to v3 - remove lightningcss

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  serverExternalPackages: ['sharp'],
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), {
-      'lightningcss': 'lightningcss',
-      '@tailwindcss/postcss': '@tailwindcss/postcss'
-    }]
-    return config
-  }
+  serverExternalPackages: ['sharp']
 }
 module.exports = nextConfig
