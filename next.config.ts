bedc07117e64cbd0655776073672c89f61977599
Adelsolimaan <info@divantrade.net>
2025-08-30 16:37:01 +0300

    fix: resolve deployment build errors

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.sanity.io', pathname: '/images/**' },
     ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['sharp']
   },
 };
 module.exports = nextConfig;
